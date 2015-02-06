5ff6010a95ad63efcf407d83ffda18eee8fa5fb4
Frank Tocci <franktocci1@gmail.com>
2015-02-05 20:53:21 -0800

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,66 @@
 
+
+
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	$(".what").click(function(){
+	$(".overlay").fadeIn(1000);
+	});
+	gameState = {};
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
+	$('#guessButton').click(function(){
+		guesser();
+		console.log("test");
+});
+	$('.new').click(function(){
+		newGame();
+	});
 
 });
 
+//call newGame when user enters number and presses enter
 
+function newGame() {
+	gameState.guessNumber = 0;
+	gameState.number = Math.floor(Math.random() * 100) + 1;	
+}
+
+function guesser() {
+	//jQuery
+	var x = gameState.number
+	var guess = parseInt($('#userGuess').val());
+	var y = Math.abs(x-guess);
+	var r = "";
+	//switch statement
+	if ((guess > 100) || (guess < 1) || (isNaN(guess) == true)) {
+		r = "please enter a number between 1 and 100";
+	}
+	else if (y >= 50) {
+		r = "Ice cold";
+	}
+	else if (y >= 30) {
+		r = "cold";
+	}
+	else if (y >= 20) {
+		r = "warm";
+	}
+	else if (y >= 10) {
+		r = "hot";
+	}
+	else if (y >= 1) {
+		r = "very hot";
+	}
+	else {
+		r = "correct!";
+	}
+	gameState.guessNumber ++;
+	//jQuery
+	document.getElementById("feedback").innerHTML = r;
+	document.getElementById("count").innerHTML = gameState.guessNumber;
+	console.log("guess = " + guess);
+	console.log("x = " + x);
+	console.log("y = " + y);
+	console.log("r = " + r);
+	//return guessNo;
+}
